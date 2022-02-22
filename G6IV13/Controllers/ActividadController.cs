ec55e3c32889bc598ef41e4708d0057a1a039ddc
hmendoza <hmendoza@ipn.mx>
2022-02-21 19:31:14 -0600

    210202

---
+++ G6IV13/Controllers/ActividadController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+/*Agregar el using para el manejo de las listas*/
+using System.Collections.Generic;
+/*Agregamos el using para modelos*/
+using G6IV13.Models;
+
+namespace G6IV13.Controllers
+{
+    public class ActividadController : Controller
+    {
+        //Se genera una lista de tipo estatica donde se
+        //almacenará los items que el usuario ingresa
+        private static List<Actividad> _actividades = new List<Actividad>();
+
+        public IActionResult Index()
+        {
+            ViewBag.Actividades = _actividades;
+            return View();
+        }
+    }
+}
---
+++ G6IV13/Models/Actividad.cs
@@ -0,0 +1,10 @@
+﻿namespace G6IV13.Models
+{
+    public class Actividad
+    {
+        /*En esta clase tenemos las tres propiedades para el manejo de la lista*/
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public int Estado { get; set; }
+    }
+}
