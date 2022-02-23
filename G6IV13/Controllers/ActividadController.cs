8cd161a8f1973fc6dc987e0f32c781aac0f89c32
hmendozaq <hmendoza@ipn.mx>
2022-02-22 18:37:55 -0600

    123

--- G6IV13/Controllers/ActividadController.cs
+++ G6IV13/Controllers/ActividadController.cs
@@ -18,10 +18,33 @@ namespace G6IV13.Controllers
             ViewBag.Actividades = _actividades;
             return View();
         }
-
+        //Cuando no se indica el método a usar, de forma predeterminada es GET
         public IActionResult Agregar()
         {
             return View();
+        }
+
+        //Se puede definir el método con el cual se recibirán los datos, en este
+        //caso es POST
+        //Los parámetros deben llamarse igual que en el formulario
+        [HttpPost]
+        public IActionResult Agregar(int ID, string Descripcion, int Estado)
+        {//Aquí hay una sobrecarga de los métodos
+            var _ID = ID;//Este ID es igual al partámetro
+            var _Descripcion = Descripcion; //Es igual a la descripción
+            var _Estado = Estado; //Es igual al Estado
+
+            /*_actividades.Add(
+                new Actividad { ID = _ID, Descripcion = _Descripcion,
+                    Estado = _Estado 
+                });*/
+            var act = new Actividad();
+            act.ID = _ID;
+            act.Descripcion = _Descripcion;
+            act.Estado = _Estado;   
+            _actividades.Add(act);
+
+            return RedirectToAction("Index");
         }
     }
 }
